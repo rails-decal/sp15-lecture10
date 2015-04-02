3da2e9d9bda229e89a3d87894d4e121e50a3199f
Sam Lau <samlau95@gmail.com>
2015-04-02 02:40:26 -0700

    Step 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,20 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
 #
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
