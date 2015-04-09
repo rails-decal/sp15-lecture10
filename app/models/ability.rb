bb133cb64aead14ee01d6f186087edc38fa14435
Howard Chen <howardxchen@gmail.com>
2015-04-09 01:32:13 -0700

    Setup ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
+    alias_action :create, :read, :update, :destroy, to: :crud
+    can :crud, Quit, user_id: user.id
+  end
+end
